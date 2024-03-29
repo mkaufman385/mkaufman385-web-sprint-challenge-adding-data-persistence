9881cdbe525535a8a73bc7160fe315f02a2cc698
mkaufman385 <mkaufman385@gmail.com>
2024-03-29 12:58:43 -0400

    wip

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,15 @@
-// build your `/api/resources` router here
+const router = require("express").Router();
+
+router.use("*", (req, res) => {
+  res.json({ api: "FROM RESOURCE ROUTER" });
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    customMessage: "Problem from RESOURCE router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
