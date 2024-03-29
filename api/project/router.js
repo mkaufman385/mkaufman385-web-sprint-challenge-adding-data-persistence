91893fc48a344e13f466d92f47a16e604cde952f
mkaufman385 <mkaufman385@gmail.com>
2024-03-29 13:42:04 -0400

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,15 @@
-// build your `/api/projects` router here
+const router = require("express").Router();
+
+router.use("*", (req, res) => {
+  res.json({ api: "FROM PROJECT ROUTER" });
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    customMessage: "Problem from PROJECT router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,15 @@
-// build your `/api/tasks` router here
+const router = require("express").Router();
+
+router.use("*", (req, res) => {
+  res.json({ api: "FROM TASK ROUTER" });
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    customMessage: "Problem from TASK router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
