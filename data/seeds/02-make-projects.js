ee2cb1447983e9b503800c1e18c446b428c5fa9b
mkaufman385 <mkaufman385@gmail.com>
2024-04-01 06:47:21 -0400

    wip

--- data/seeds/02-make-projects.js
+++ data/seeds/02-make-projects.js
@@ -1,1 +1,12 @@
-exports.seed = function (knex) {};
+const projects = [
+  {
+    project_id: 1,
+    project_name: "bar",
+    project_description: null,
+    project_completed: false,
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex("projects").insert(projects);
+};
