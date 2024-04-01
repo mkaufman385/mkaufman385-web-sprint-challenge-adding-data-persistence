b0bc0d21a8de1a52f4d8c5a8a5cd16ae91e3fb10
mkaufman385 <mkaufman385@gmail.com>
2024-04-01 08:42:59 -0400

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,5 +1,10 @@
-function getProjectById() {
-  return Promise.resolve("Impressive project from projects model");
+const db = require("../../data/dbConfig");
+
+async function getProjectById() {
+  const projectRows = await db("project as p");
+  // .where("project_id", project_id);
+
+  return projectRows;
 }
 
 module.exports = { getProjectById };
--- data/migrations/01-projects-20240329190230_initial-migration.js
+++ data/migrations/01-projects-20240329190230_initial-migration.js
@@ -1,9 +1,9 @@
 exports.up = async function (knex) {
   await knex.schema.createTable("project", (table) => {
-    table.increments("project_id");
+    table.increments("project_id").primary();
     table.string("project_name", 128).notNullable();
     table.string("project_description");
-    table.string("project_completed").defaultTo(0);
+    table.boolean("project_completed").defaultTo(false);
   });
 };
 
