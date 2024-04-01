ed51c902508e5d796aed3347e546995fd5eeaa36
mkaufman385 <mkaufman385@gmail.com>
2024-04-01 18:50:00 -0400

    wip

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,8 @@
-// build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+async function getAll() {
+  // return db("resources");
+  return Promise.resolve("awesome resource from resource model");
+}
+
+module.exports = { getAll };
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const Resource = require("./model");
 
-router.use("*", (req, res) => {
-  res.json({ api: "resource router working!" });
+router.get("/", (req, res, next) => {
+  Resource.getAll()
+
+    .then((resources) => {
+      res.status(200).json(resources);
+    })
+    .catch(next);
 });
 
 router.use((err, req, res, next) => {
