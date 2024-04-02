330bc1f4bd4d11e6591893715252d3476c7f50a1
mkaufman385 <mkaufman385@gmail.com>
2024-04-02 15:09:42 -0400

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,7 +1,18 @@
 const db = require("../../data/dbConfig");
 
 async function getAll() {
-  return db("projects");
+  // return db("projects");
+  const projects = await db("projects").select(
+    "project_id",
+    "project_name",
+    "project_description",
+    "project_completed"
+  );
+
+  return projects.map((project) => ({
+    ...project,
+    project_completed: project.project_completed === 1 ? true : false,
+  }));
 }
 
 module.exports = { getAll };
