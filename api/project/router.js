c3ce641a287a3d2519a07da18005d64bea83e01f
mkaufman385 <mkaufman385@gmail.com>
2024-04-02 16:33:10 -0400

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Project = require("./model");
+const db = require("../../data/dbConfig");
 
 router.get("/", (req, res, next) => {
   Project.getAll()
@@ -8,6 +9,35 @@ router.get("/", (req, res, next) => {
       res.status(200).json(projects);
     })
     .catch(next);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { project_name, project_description, project_completed } = req.body;
+
+    // Checking if project_name is provided
+    if (!project_name) {
+      return res.status(400).json({ message: "Project name is required." });
+    }
+
+    // Inserting new project into the database
+    const [project_id] = await db("projects").insert({
+      project_name,
+      project_description,
+      project_completed: project_completed ? 1 : 0, // Convert boolean to integer
+    });
+
+    // Fetching the newly created project
+    const project = await db("projects").where({ project_id }).first();
+
+    // Converting project_completed to boolean
+    project.project_completed = !!project.project_completed;
+
+    // Responding with the newly created project
+    res.status(201).json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 router.use((err, req, res, next) => {
