cc45b815d8079596e0f4a20178dc75b15cfd5303
mkaufman385 <mkaufman385@gmail.com>
2024-04-02 15:17:35 -0400

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,19 @@
 const db = require("../../data/dbConfig");
 
 async function getAll() {
-  return db("tasks");
+  // return db("tasks");
+  const tasks = await db("tasks").select(
+    "task_id",
+    "task_description",
+    "task_notes",
+    "task_completed"
+  );
+
+  // Convert task_completed to boolean using ternary operator
+  return tasks.map((task) => ({
+    ...task,
+    task_completed: task.task_completed === 1 ? true : false,
+  }));
 }
 
 module.exports = { getAll };
